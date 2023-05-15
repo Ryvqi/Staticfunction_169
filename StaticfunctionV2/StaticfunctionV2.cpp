2d7f902113afeac9e2e6941143e81d6662b1968e
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-15 16:04:52 +0700

    intmahasiswa

--- StaticfunctionV2/StaticfunctionV2.cpp
+++ StaticfunctionV2/StaticfunctionV2.cpp
@@ -17,3 +17,33 @@ public:
 
 	mahasiswa(string pNama) :nama(pNama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printALL() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+	system("pause");
+
+	return 0;
+}
