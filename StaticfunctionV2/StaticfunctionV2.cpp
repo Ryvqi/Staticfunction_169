ac5607ab3bb92742dda10942d81a3f3f3cba2307
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-15 15:52:00 +0700

    class mahasiswa

---
+++ StaticfunctionV2/StaticfunctionV2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+
+	static void setNim(int pNim) { nim = pNim; }
+	static int getNim() { return nim; }
+
+	mahasiswa(string pNama) :nama(pNama) { setID(); }
+};
