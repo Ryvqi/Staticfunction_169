1f65e9d0091fdbb97040e75b7fbccdad9771745a
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-15 15:39:36 +0700

    void mahasiswa

--- Staticfunction_169/Staticfunction_169.cpp
+++ Staticfunction_169/Staticfunction_169.cpp
@@ -20,6 +20,8 @@ void mahasiswa::setID() {
 }
 
 void mahasiswa::printALL() {
-	cout << "ID : " << id << endl;
-	
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
 }
+
